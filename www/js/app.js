1183c6b8209300fda0bc7c439c0ea48e1d862f40
leoz2011 <leoz@yahoo.com>
2014-09-21 01:15:48 -0400

    Separate application route

--- www/js/app.js
+++ www/js/app.js
@@ -1,72 +1,19 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
+// Blogga LiveJournal client
 
-angular.module('starter', ['ionic', 'MainCtrl', 'JournalCtrl', 'PostCtrl'])
+angular.module('blogga', ['ionic', 'MainRoute', 'MainCtrl', 'JournalCtrl', 'PostCtrl'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-      url: "/app",
-      abstract: true,
-      templateUrl: "templates/menu.html",
-      controller: 'MainController'
-    })
-
-    .state('app.search', {
-      url: "/search",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/search.html"
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
-      }
-    })
-
-    .state('app.browse', {
-      url: "/browse",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/browse.html"
+        if(window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
         }
-      }
-    })
-    .state('app.journal', {
-      url: "/journal/:journalId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/journal.html",
-          controller: 'JournalController'
-        }
-      }
-    })
-
-    .state('app.single', {
-      url: "/journal/:journalId/:postId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/post.html",
-          controller: 'PostController'
-        }
-      }
     });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/journal/3');
 });
 
---
+++ www/js/route.js
@@ -0,0 +1,54 @@
+
+angular.module('MainRoute', []).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+$stateProvider
+
+    .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'MainController'
+    })
+
+    .state('app.search', {
+        url: "/search",
+        views: {
+            'menuContent' :{
+                templateUrl: "templates/search.html"
+            }
+        }
+    })
+
+    .state('app.browse', {
+        url: "/browse",
+        views: {
+            'menuContent' :{
+                templateUrl: "templates/browse.html"
+            }
+        }
+    })
+    
+    .state('app.journal', {
+        url: "/journal/:journalId",
+            views: {
+            'menuContent' :{
+                templateUrl: "templates/journal.html",
+                controller: 'JournalController'
+            }
+        }
+    })
+
+    .state('app.single', {
+        url: "/journal/:journalId/:postId",
+            views: {
+            'menuContent' :{
+                templateUrl: "templates/post.html",
+                controller: 'PostController'
+            }
+        }
+    });
+    
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/journal/3');
+
+}]);
+
