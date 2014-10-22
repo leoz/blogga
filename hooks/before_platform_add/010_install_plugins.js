b40f7fe4d4d946fdd8dd91010b85576febd351b1
leoz2011 <leoz@yahoo.com>
2014-10-21 22:44:42 -0400

    Cordova plugin hook

---
+++ hooks/before_platform_add/010_install_plugins.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+//this hook installs all your plugins
+
+// add your plugins to this list--either the identifier, the filesystem location or the URL
+var pluginlist = [
+    "org.apache.cordova.splashscreen",
+    "org.apache.cordova.statusbar",
+    "org.apache.cordova.device",
+    "org.apache.cordova.file",
+    "org.apache.cordova.file-transfer"
+];
+
+// no need to configure below
+
+var fs = require('fs');
+var path = require('path');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+function puts(error, stdout, stderr) {
+    sys.puts(stdout)
+}
+
+pluginlist.forEach(function(plug) {
+    exec("cordova plugin add " + plug, puts);
+});
+
