527f9c552c622e0e1fb2903e2ee2569065edc445
leoz2011 <leoz@yahoo.com>
2014-12-30 17:44:46 -0500

    Recover bookmarks selection

--- www/app/bookmarks/bookmarksctrl.js
+++ www/app/bookmarks/bookmarksctrl.js
@@ -1,12 +1,18 @@
 
 angular.module('BookmarksCtrl', [])
-.controller('BookmarksController', function($scope, BookmarksService) {
+.controller('BookmarksController', function($scope, $location, BookmarksService) {
 
     $scope.service = BookmarksService;
 
     $scope.canDelete = false;
 
     $scope.data = { name: null };
+
+    $scope.isItemActive = function(journalName) {
+        var href = "/app/journal/" + journalName;
+        var path = $location.path();
+        return path.indexOf(href) > -1;
+    };
 
     $scope.toggleShowDelete = function(){
         $scope.canDelete = !$scope.canDelete;
