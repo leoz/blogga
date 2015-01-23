8471f029561d1c534b0465c8971a7786e3110470
Leonid Zolotarev <lzolotarev@blackberry.com>
2015-01-22 19:24:38 -0500

    CORS fix

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ app.use(express.static('www'));
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
     next();
 });
 
